83b422b1756924d249fca5e87d0746ec2fcee2cb
Ved Kothavade <ved@kothavade.com>
2024-07-12 01:19:16 -0400

    cc cli

--- shell.nix
+++ shell.nix
@@ -1,9 +1,32 @@
 {
   pkgs ? import <nixpkgs> { },
 }:
+let
+  cc = pkgs.stdenv.mkDerivation rec {
+    name = "codecrafters-cli-${version}";
+    version = "33";
+
+    # https://github.com/codecrafters-io/cli/releases
+    src = pkgs.fetchurl {
+      url = "https://github.com/codecrafters-io/cli/releases/download/v${version}/v${version}_darwin_arm64.tar.gz";
+      sha256 = "sha256-vBaLSW/5inanndfdiImPJF+pAhXtM1XALfz0bWP7N9k=";
+    };
+
+    unpackPhase = ''
+      mkdir -p $out
+      tar -xzvf ${src}
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp -r . $out/bin/
+    '';
+  };
+in
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     curl
     clang-tools
+    cc
   ];
 }
