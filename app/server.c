55c9a7ecd6975b9ec77db2801e3dcafcb0b137cb
Ved Kothavade <ved@kothavade.com>
2024-07-14 18:51:27 -0400

    file getter and directory input

--- app/server.c
+++ app/server.c
@@ -2,26 +2,37 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <pthread.h>
 #include <unistd.h>
-#include <stdbool.h>
 
 #include "request.h"
 
+#define HTTP_OK "HTTP/1.1 200 OK\r\n"
+
 const char ECHO_TARGET[] = "/echo/";
 const char USER_TARGET[] = "/user-agent";
+const char FILE_TARGET[] = "/files/";
 
-void* handleClient(void* arg);
+const char DIR_ARG[] = "--directory";
+
+void *handleClient(void *arg);
 void ok(Request *req);
 void echo(Request *req);
 void userAgent(Request *req);
+void file(Request *req);
 void notFound(Request *req);
 
-int main() {
+char *dir = "./";
+
+int main(int argc, char **argv) {
+    if ((argc >= 2) && (strncmp(argv[1], DIR_ARG, strlen(DIR_ARG)) == 0)) {
+        dir = argv[2];
+    }
     // Disable output buffering
     setbuf(stdout, NULL);
     setbuf(stderr, NULL);
@@ -78,8 +89,8 @@ int main() {
     return 0;
 }
 
-void* handleClient(void* arg){
-    int client_fd = *((int*)arg);
+void *handleClient(void *arg) {
+    int client_fd = *((int *)arg);
     free(arg);
 
     char input[1024];
@@ -117,6 +128,8 @@ void* handleClient(void* arg){
         echo(&req);
     } else if (strncmp(USER_TARGET, req.target, strlen(USER_TARGET)) == 0) {
         userAgent(&req);
+    } else if (strncmp(FILE_TARGET, req.target, strlen(FILE_TARGET)) == 0) {
+        file(&req);
     } else {
         notFound(&req);
     }
@@ -127,43 +140,68 @@ void* handleClient(void* arg){
 }
 
 void ok(Request *req) {
-    const char OK[] = "HTTP/1.1 200 OK\r\n\r\n";
+    const char OK[] = HTTP_OK "\r\n";
     write(req->client_fd, OK, strlen(OK));
 }
 
 void echo(Request *req) {
-    const char ECHO_PREFIX[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
+    const char ECHO_PREFIX[] = HTTP_OK
+        "Content-Type: text/plain\r\n"
+        "Content-Length: ";
 
     const char *input = req->target + strlen(ECHO_TARGET);
     int input_len = strlen(input);
-    char *echo = malloc(
-        strlen(ECHO_PREFIX)
-        // input-length length
-        + snprintf(NULL, 0, "%d", input_len)
-        + strlen("\r\n\r\n")
-        + strlen(input)
-        + 1);
+    char *echo = malloc(strlen(ECHO_PREFIX) + snprintf(NULL, 0, "%d", input_len) +
+                        strlen("\r\n\r\n") + strlen(input) + 1);
     sprintf(echo, "%s%d\r\n\r\n%s", ECHO_PREFIX, input_len, input);
     write(req->client_fd, echo, strlen(echo));
     free(echo);
 }
 
 void userAgent(Request *req) {
-    const char USER_PREFIX[] = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ";
+    const char USER_PREFIX[] = HTTP_OK
+        "Content-Type: text/plain\r\n"
+        "Content-Length: ";
     Header *h = getHeader(req, "User-Agent");
     char *body = h->body;
     int body_len = strlen(body);
 
-    char *user_agent = malloc(
-        strlen(USER_PREFIX)
-        // input-length length
-        + snprintf(NULL, 0, "%d", body_len)
-        + strlen("\r\n\r\n")
-        + strlen(body)
-        + 1);
+    char *user_agent = malloc(strlen(USER_PREFIX) + snprintf(NULL, 0, "%d", body_len) +
+                              strlen("\r\n\r\n") + strlen(body) + 1);
     sprintf(user_agent, "%s%d\r\n\r\n%s", USER_PREFIX, body_len, body);
     write(req->client_fd, user_agent, strlen(user_agent));
     free(user_agent);
+}
+
+void file(Request *req) {
+    const char FILE_PREFIX[] = HTTP_OK
+        "Content-Type: application/octet-stream\r\n"
+        "Content-Length: ";
+    const char *input = req->target + strlen(FILE_TARGET);
+    char path[1024];
+
+    strcpy(path, dir);
+    strcpy(path + strlen(dir), input);
+
+    FILE *fp = fopen(path, "r");
+    if (fp == NULL) {
+        notFound(req);
+        return;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    char *get_file = malloc(strlen(FILE_PREFIX) + snprintf(NULL, 0, "%ld", size) +
+                            strlen("\r\n\r\n") + size + 1);
+    sprintf(get_file, "%s%ld\r\n\r\n", FILE_PREFIX, size);
+
+    fread(get_file + strlen(get_file), 1, size, fp);
+    write(req->client_fd, get_file, strlen(get_file));
+
+    free(get_file);
+    fclose(fp);
 }
 
 void notFound(Request *req) {
